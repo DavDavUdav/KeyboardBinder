62459d7d56f65a430790fa08f7a0f7806c0f2a11
DavDavUdav <stepan05az@gmail.com>
2024-10-17 22:59:11 +0300

    Добавьте файлы проекта.

---
+++ KeyboardBinder/KeyboardBinder.cpp
@@ -0,0 +1,66 @@
+#include <Windows.h>
+
+// Global variable.
+HHOOK KeyboardHook;
+
+
+LRESULT CALLBACK LLKeyboardHook(int nCode, WPARAM wParam, LPARAM lParam)
+{
+	if (nCode == HC_ACTION)
+	{
+		KBDLLHOOKSTRUCT* p = (KBDLLHOOKSTRUCT*)lParam;
+		if (p->vkCode == VK_CAPITAL && wParam == WM_KEYDOWN)
+		{
+			INPUT inputs[4] = {};
+
+			inputs[0].type = INPUT_KEYBOARD;
+			inputs[0].ki.wVk = VK_LWIN;
+
+			inputs[1].type = INPUT_KEYBOARD;
+			inputs[1].ki.wVk = VK_SPACE;
+
+			inputs[2].type = INPUT_KEYBOARD;
+			inputs[2].ki.wVk = VK_LWIN;
+			inputs[2].ki.dwFlags = KEYEVENTF_KEYUP;
+
+			inputs[3].type = INPUT_KEYBOARD;
+			inputs[3].ki.wVk = VK_SPACE;
+			inputs[3].ki.dwFlags = KEYEVENTF_KEYUP;
+
+			SendInput(ARRAYSIZE(inputs), inputs, sizeof(INPUT));
+			return 1;
+		}
+	}
+	// Null тк автоматически беретс€ текущий хук.
+	return CallNextHookEx(NULL, nCode, wParam, lParam);
+}
+
+/// <summary>
+/// https://learn.microsoft.com/en-us/windows/win32/api/winbase/nf-winbase-winmain
+/// </summary>
+/// <param name="hInstance"> - ƒескриптор текущего приложени€. </param>
+/// <param name="hPrevInstance"> - ƒескриптор предыдущего экземпл€ра приложени€. </param>
+/// <param name="lpCmdLine"> - јргументы командной строки без приложени€. </param>
+/// <param name="nShowCmd"> - ”правление отображением окна. </param>
+/// <returns></returns>
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+	// ”казываем тип хука, куда передаем управление хуком, указываем инстанс приложени€ чтобы система знала кто ставит хук, и NULL дл€ установки хука глобально.
+	KeyboardHook = SetWindowsHookExW(WH_KEYBOARD_LL, LLKeyboardHook, hInstance, NULL);
+
+	MSG message;
+	BOOL bRet;
+	while ((bRet = GetMessage(&message,NULL, 0,0)) != 0)
+	{
+		if (bRet == -1)
+		{
+
+		}
+		else
+		{
+			TranslateMessage(&message);
+			DispatchMessage(&message);
+		}
+	}
+	UnhookWindowsHookEx(KeyboardHook);
+}
